d1156e81e40640ef11890f739a95a093b666f275
Hojung Youn <amoc.yn@gmail.com>
2013-01-10 12:13:33 +0900

    Move Archive::Npk package to Archive::Npk::API [3/3]
    Add fresh Archive::Npk package which is just empty now.

---
+++ lib/Archive/Npk.pm
@@ -0,0 +1,24 @@
+package Archive::Npk;
+use warnings;
+use strict;
+
+use Archive::Npk::API;
+
+our $VERSION = "0.000001_001";
+
+1;
+__END__
+
+=head1 NAME
+
+Archive::Npk - Neat Package System - npk
+
+=head1 SYNOPSIS
+
+  use Archive::Npk;
+
+=head1 DESCRIPTION
+
+blah blah
+
+=cut
