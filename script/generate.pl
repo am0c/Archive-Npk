deaa5dd52b46c5712077105c144e41ff7f836d3a
Hojung Youn <amoc.yn@gmail.com>
2013-02-19 21:49:56 +0900

    Bless AST objects with proper class names attached for comprehension

--- script/generate.pl
+++ script/generate.pl
@@ -184,7 +184,7 @@ sub fdecl_parse {
                 }
             }
             continue {
-                push @args, [ @arg ];
+                push @args, bless [ @arg ], "Archive::Npk::fdecl_arg";
                 if (look(value => ',')) {
                     match();
                 }
@@ -255,7 +255,7 @@ sub fdecl_tokenize {
     }{
         my ($idx) = keys %+;
         die "Parse error near $+{error}" if exists $+{error};
-        push @token, +{ token_type => $idx, value => $& } if $idx;
+        push @token, bless +{ token_type => $idx, value => $& }, "Archive::Npk::fdecl_token" if $idx;
         1;
     }xsge;
 
